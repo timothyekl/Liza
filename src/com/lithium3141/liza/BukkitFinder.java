324adfc57b3b9fc19cf5e263329fbe57847b5403
Tim Ekl <lithium3141@gmail.com>
2011-04-29 02:44:44 -0400

    Refactor out ugliness into BukkitFinder class

---
+++ src/com/lithium3141/liza/BukkitFinder.java
@@ -0,0 +1,95 @@
+package com.lithium3141.liza;
+
+import java.lang.reflect.Field;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.ThreadServerApplication;
+
+/**
+ * Aids in the process of finding a Bukkit. (In reality, does some
+ * black magic with threads and reflection to get the running
+ * MinecraftServer instance.)
+ * 
+ * @author Tim Ekl <lithium3141@gmail.com>
+ */
+public class BukkitFinder {
+	/**
+	 * The name given to the thread running the primary server. Used
+	 * to distinguish between active threads and locate the one
+	 * handling the game, as opposed to e.g. the net listener.
+	 */
+	public static final String MINECRAFT_SERVER_THREAD_NAME = "Server thread";
+	
+	/**
+	 * The obfuscated name used in the ThreadServerApplication class
+	 * to refer to the active MinecraftServer instance. Used in
+	 * later reflection to locate and fetch that instance.
+	 */
+	public static final String MINECRAFT_SERVER_FIELD_NAME = "a";
+	
+	/**
+	 * Obtain an array of all active threads in the current Java VM.
+	 * Handy for doing quick iteration over threads later to locate
+	 * an active MinecraftServer instance.
+	 * 
+	 * @return an array of all Thread objects currently registered in
+	 * the running Java VM.
+	 */
+	public static Thread[] getAllThreads() {
+		ThreadGroup rootGroup = Thread.currentThread( ).getThreadGroup( );
+		ThreadGroup parentGroup;
+		while ( ( parentGroup = rootGroup.getParent() ) != null ) {
+		    rootGroup = parentGroup;
+		}
+		Thread[] threads = new Thread[ rootGroup.activeCount() ];
+		while ( rootGroup.enumerate( threads, true ) == threads.length ) {
+		    threads = new Thread[ threads.length * 2 ];
+		}
+		return threads;
+	}
+	
+	/**
+	 * Locate the MinecraftServer instance currently running in this
+	 * Java VM. Relies on both the label given to the server thread
+	 * and the obfuscated field name in ThreadServerApplication, as
+	 * well as the getAllThreads() method.
+	 * 
+	 * This method performs some serious black magic with regards to
+	 * threading and reflection, and is highly brittle. Use with
+	 * caution. Also note that in order to succeed, the server must
+	 * have been spawned as a Thread within the same VM from which
+	 * getActiveMinecraftServer() is being called.
+	 * 
+	 * @see ThreadServerApplication
+	 * @see BukkitFinder#getAllThreads()
+	 * @return the active MinecraftServer instance, or null if such
+	 * an instance cannot be found
+	 */
+	public static MinecraftServer getActiveMinecraftServer() {
+		// Loop over active threads
+		for(Thread t : getAllThreads()) {
+			// Find thread labeled properly
+			if(t != null && t.getName() != null && t.getName().equals(MINECRAFT_SERVER_THREAD_NAME)) {
+				ThreadServerApplication tsa = (ThreadServerApplication)t;
+				
+				// Do reflection black magic
+				Class<? extends ThreadServerApplication> c = tsa.getClass();
+				Field f;
+				try {
+					f = c.getDeclaredField(MINECRAFT_SERVER_FIELD_NAME);
+					
+					// Make field accessible to this method, then get value
+					f.setAccessible(true);
+					MinecraftServer mcserver = (MinecraftServer) f.get(tsa);
+					return mcserver;
+				} catch (Exception e) {
+					// Pokemon exception handling, I know, but this whole thing smells anyway
+					return null;
+				}
+			}
+		}
+		
+		// Couldn't find an active server
+		return null;
+	}
+}
--- src/com/lithium3141/liza/CBThread.java
+++ src/com/lithium3141/liza/CBThread.java
@@ -2,10 +2,6 @@ package com.lithium3141.liza;
 
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.lang.reflect.Field;
-
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.ThreadServerApplication;
 
 public class CBThread extends Thread {
 	public InputStream in;
@@ -30,41 +26,6 @@ public class CBThread extends Thread {
 	@Override
 	public void interrupt() {
 		super.interrupt();
-		
-		Main.stdout.println("I mean to be stopped...");
-		
-		ThreadGroup rootGroup = Thread.currentThread( ).getThreadGroup( );
-		ThreadGroup parentGroup;
-		while ( ( parentGroup = rootGroup.getParent() ) != null ) {
-		    rootGroup = parentGroup;
-		}
-		Thread[] threads = new Thread[ rootGroup.activeCount() ];
-		while ( rootGroup.enumerate( threads, true ) == threads.length ) {
-		    threads = new Thread[ threads.length * 2 ];
-		}
-		for(Thread t : threads) {
-			if(t != null && t.getName() != null && t.getName().equals("Server thread")) {
-				Main.stdout.println("Found server thread: " + t.toString() + " " + t.getClass());
-				ThreadServerApplication tsa = (ThreadServerApplication)t;
-				
-				// Do reflection black magic
-				Class<? extends ThreadServerApplication> c = tsa.getClass();
-				Field f;
-				try {
-					f = c.getDeclaredField("a");
-					f.setAccessible(true);
-					MinecraftServer mcserver = (MinecraftServer) f.get(tsa);
-					mcserver.stop();
-				} catch (SecurityException e) {
-					e.printStackTrace();
-				} catch (NoSuchFieldException e) {
-					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
-					e.printStackTrace();
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				}
-			}
-		}
+		BukkitFinder.getActiveMinecraftServer().stop();
 	}
 }
