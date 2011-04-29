f73b4f3b4cb2579b002fe441355d9082ce647d41
Tim Ekl <lithium3141@gmail.com>
2011-04-29 16:38:04 -0400

    Push I/O stream reassignment down into thread

--- src/com/lithium3141/liza/CBThread.java
+++ src/com/lithium3141/liza/CBThread.java
@@ -1,5 +1,7 @@
 package com.lithium3141.liza;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.PrintStream;
 
@@ -7,19 +9,24 @@ public class CBThread extends Thread {
 	public InputStream in;
 	public PrintStream out;
 	public PrintStream err;
-	public String[] options;
+	public String[] options = {"-h", "localhost", "-p", "31415"};
 	
-	public CBThread(InputStream in, PrintStream out, PrintStream err, String[] options) {
+	public CBThread() {
 		this.in = in;
-		this.out = out;
-		this.err = err;
-		this.options = options;
+		try {
+			this.out = new PrintStream(new File("liza-output.txt"));
+			this.err = new PrintStream(new File("liza-error.txt"));
+		} catch (FileNotFoundException e) {
+			Main.stderr.println("Could not assign new output streams for Liza!");
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 	
 	public void run() {
-		System.setOut(out);
-		System.setIn(in);
-		System.setErr(err);
+		System.setOut(this.out);
+		System.setIn(this.in);
+		System.setErr(this.err);
 		org.bukkit.craftbukkit.Main.main(options);
 	}
 	
--- src/com/lithium3141/liza/Main.java
+++ src/com/lithium3141/liza/Main.java
@@ -1,8 +1,6 @@
 package com.lithium3141.liza;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,20 +11,12 @@ public class Main {
 
 	public static CBThread serverThread;
 	
-	final static String[] serverOptions = {"-h", "localhost", "-p", "31415"};
-	
 	public static PrintStream stdout;
 	public static PrintStream stderr;
 	public static InputStream stdin;
 	
-	public static PrintStream serverOut;
-	public static PrintStream serverErr;
-	public static InputStream serverIn;
-	
-	public static boolean shouldTerminate;
-	
 	/**
-	 * Launch the test runner.
+	 * Launch the test runner semi-interactively.
 	 * @param args Command-line arguments
 	 * @throws IOException if System.in is broken
 	 */
@@ -36,22 +26,9 @@ public class Main {
 		stderr = System.err;
 		stdin = System.in;
 		
-		// Set up streams
-		try {
-			serverOut = new PrintStream(new File("test-output.txt"));
-			serverErr = new PrintStream(new File("test-error.txt"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		serverIn = System.in;
-		
 		// Run CB
-		serverThread = new CBThread(serverIn, serverOut, serverErr, serverOptions);
+		serverThread = new CBThread();
 		serverThread.start();
-		
-		// Check streams
-		System.out.println("test");
 		
 		// Sleep for thread
 		while(true) {
